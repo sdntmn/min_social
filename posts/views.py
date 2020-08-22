57ea9ca9d4b8f27d98be05804fbdc2d6c9159fbc
sdntmn <sdntmn@mail.ru>
2020-08-22 17:43:42 +0500

    1
    12

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse, request
 
 from .models import Post, Group
-from django.forms import ModelForm
+from django.forms import PostForm
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
@@ -18,10 +18,12 @@ def group_posts(request, slug):
 
 def new_post(reguest):
     if request.method == "POST":
-        form = ModelForm(reguest.POST)
+        form = PostForm(reguest.POST)
         if form.is_valid():
-            form.save()
-            return redirect("")
-        return render(request, "new_post.html", {"form": form})
-    form = ModelForm(reguest.POST)   
-    return render(request, "new_post.html", {"form": form})
+            post_get = form.save(commit=False)
+            post_get.author = request.user
+            post_get.save()
+            return redirect("index")
+        else:
+            return render(request, 'new.html')
+    return render(request, "new.html", {"form": form})

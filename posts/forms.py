ba0ca6ed6bce97418dd2261f5f64f5fa8b1ac1a5
sdntmn <67434735+sdntmn@users.noreply.github.com>
2020-11-04 21:32:59 +0500

    1
    1

---
+++ posts/forms.py
@@ -0,0 +1,13 @@
+
+from django.contrib.auth import get_user_model
+from django import forms
+
+from posts.models import Post
+
+User = get_user_model()
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['group', 'text']
---
+++ posts/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(
+                    verbose_name="Наименование группы",
+                    max_length=200)
+    slug = models.SlugField(
+                    verbose_name='Короткое имя',
+                    unique=True,
+                    null=True)
+    description = models.TextField('Описание')
+
+    class Meta:
+        verbose_name = "Группу"
+        verbose_name_plural = "Группы" 
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(
+                    verbose_name="Текст")
+    pub_date = models.DateTimeField(
+                    verbose_name="Дата публикации",
+                    auto_now_add=True)
+    author = models.ForeignKey(
+                    User,
+                    on_delete=models.CASCADE,
+                    related_name="posts",
+                    verbose_name="Автор")
+    group = models.ForeignKey(
+                    Group,
+                    on_delete=models.SET_NULL,
+                    related_name="posts",
+                    verbose_name="Группа",
+                    blank=True,
+                    null=True)
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+    def __str__(self):
+        return 'Группы - %s : Автора - %s : Опубликованный %s : Текст(кратко): %s' % (
+            self.group,
+            self.author,
+            self.pub_date,
+            self.text[0:50:])
